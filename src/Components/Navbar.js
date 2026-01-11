cee2585d350decaed2b9a0ff02c3ca393c1a0dec
Nandhini-R-2003 <146432839+Nandhini-R-2003@users.noreply.github.com>
2026-01-11 11:25:12 +0530

    Change navbar brand name to 'NANDHINI'

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar(){
     return(
         <nav class="navbar navbar-expand-lg bg-danger">
           <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="#">NANDHINI</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
